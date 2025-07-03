790a17c1070ab89bf1643cecffc5632e11347b40
logandapp <logandapp@gmail.com>
2025-07-03 17:44:11 -0400

    Implemented aliases for float to better hint temperature and pressure.

--- src/caldera/types/__init__.py
+++ src/caldera/types/__init__.py
@@ -1,1 +1,2 @@
 from .phase_flag import PhaseState, PhaseFlag
+from .scalars import Temperature, Pressure, SpecificVolume, SpecificEnthalpy, SpecificEntropy, SpecificInternalEnergy
---
+++ src/caldera/types/scalars.py
@@ -0,0 +1,10 @@
+from pydantic import Field
+from typing import Annotated
+
+Temperature = Annotated[float, Field("Temperature in K (kelvin)")]
+Pressure = Annotated[float, Field("Pressure in Pa (pascals)")]
+
+SpecificVolume = Annotated[float, Field("Specific Volume in m^3/kg")]
+SpecificEnthalpy = Annotated[float, Field("Specific Enthalpy in J/kg")]
+SpecificInternalEnergy = Annotated[float, Field("Specific Internal Energy in J/kg")]
+SpecificEntropy = Annotated[float, Field("Specific Entropy in J/(kg K)")]
